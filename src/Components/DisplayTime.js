cae780dcc3dee160ab1abd9a35fa758690124f7e
Digbijaya Biswal <digbijaya.biswal1234@gmail.com>
2021-09-25 22:41:08 +0530

    name convension fixed

--- src/Components/DisplayTime.js
+++ src/Components/DisplayTime.js
@@ -3,21 +3,21 @@ import { useState } from "react";
 
 const DisplayTime = () => {
     let time = new Date().toLocaleTimeString();
-    let newTime = Math.floor(new Date().getTime() / 1000.0)
+    let checkSec = Math.floor(new Date().getTime() / 1000.0)
     const [currentTime, setCurrentTime] = useState(time);
 
-    const UpdateTime = () => {
+    const updateTime = () => {
         time = new Date().toLocaleTimeString();
         console.log('time==>', time);
         setCurrentTime(time);
     }
 
-    setInterval(UpdateTime, 1000);
+    setInterval(updateTime, 1000);
     return (
         <div>
             <h2>Digital Clock</h2>
             <center>
-                <div style={{ background: `${newTime % 2 === 0 ? 'black' : 'white'}`, color: `${newTime % 2 === 0 ? 'white' : 'black'}`, width: 'fit-content', padding: '10px',borderRadius:'15px' }}>
+                <div style={{ background: `${checkSec % 2 === 0 ? 'black' : 'white'}`, color: `${checkSec % 2 === 0 ? 'white' : 'black'}`, width: 'fit-content', padding: '10px',borderRadius:'15px' }}>
                     <h3>{currentTime}</h3>
                 </div>
             </center>
